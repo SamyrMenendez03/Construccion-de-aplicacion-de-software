9051098b4dc8453e9b30a80b5f4fe7f96a88f096
SamyrMenendez03 <tmenendezc@est.ups.edu.ec>
2026-01-10 14:47:20 -0500

    Tarea 02.04: Documentacion y anotacion en Repositorio

--- src/main/java/com/grupoA/backend/repository/CategoriaRepository.java
+++ src/main/java/com/grupoA/backend/repository/CategoriaRepository.java
@@ -2,6 +2,10 @@ package com.grupoA.backend.repository;
 
 import com.grupoA.backend.model.Categoria;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
+    // No es necesario implementar metodos, Spring Data JPA lo hace automaticamente.
 }
